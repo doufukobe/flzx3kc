284eb1510413bb227b73dbd085fa5eed390fc23b
gongdapeng <gongdapeng@bytedance.com>
2018-03-15 17:32:47 +0800

    feat(个人闯关页): 完成modal和toast

--- src/pages/single-detail/single-detail.js
+++ src/pages/single-detail/single-detail.js
@@ -9,12 +9,45 @@ Page({
     idiomSrc: '../../images/single-detail/idiom-bg.png', // 成语背景图链接
     idiomBgSrc: '../../images/single-detail/idiom-bg-large.png', // 通用背景链接
     downloadBgSrc: '../../images/single-detail/idiom-bg-down.png', // 底部吸底图片
-    inputIdiom: ['夸', '父', '追', ''], // 输入成语数据源
+    inputIdiom: ['夸', '父', '追', '日'], // 输入成语数据源
     selectChars: [
       '連', '膽', '夸', '話', '費', '級', '抗', '為',
       '夸', '輕', '木', '精', '頭', '不', '騙', '蛇',
       '為', '追', '日', '這', '片', '哈', '中', '尾'
     ],
+    hideIndex: [], // 隐藏字符对应 selectChars 索引
+    isSuccess: false, // 标注成语是否答对
+    isError: false, // 标注是否展示错误 toast
+    // errorMsg: '存在错误哦！',
+    errorMsg: '先删除错误答案',
+  },
+
+  // 时间处理函数
+  handleTap(e) {
+    const hideIndex = this.data.hideIndex;
+    hideIndex.push(e.target.dataset.index);
+    this.setData({
+      hideIndex
+    });
+    console.log(this.data.hideIndex);
+
+    this.toastSuccess();
+  },
+  toastSuccess() {
+    this.setData({
+      isSuccess: !this.data.isSuccess,
+    });
+  },
+  toastError() {
+    this.setData({
+      isError: true,
+    });
+
+    setTimeout(() => {
+      this.setData({
+        isError: false,
+      });
+    }, 1500);
   },
 
   /**
