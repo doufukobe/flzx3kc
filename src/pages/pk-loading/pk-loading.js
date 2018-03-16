b09037e9d0452e1b7a07515801a9c4f159759842
gongdapeng <gongdapeng@bytedance.com>
2018-03-16 11:38:25 +0800

    feat(page): 完成个人闯关交互及pk匹配静态页面

---
+++ src/pages/pk-loading/pk-loading.js
@@ -0,0 +1,77 @@
+// pages/pk-loading/pk-loading.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    isMatched: true, // 是否匹配成功
+    passedTime: 3, // 已等待时间
+    currentUser: {
+      name: '葱头豆瓣酱',
+      record: 10,
+      avatar: 'https://sf3-ttcdn-tos.pstatp.com/img/game-files/16393a4b709356457ad45282f6d1e873.jpeg~110x110.jpeg'
+    },
+    opponent: {
+      name: '葱头豆瓣酱',
+      record: 10,
+      avatar: 'https://sf3-ttcdn-tos.pstatp.com/img/game-files/16393a4b709356457ad45282f6d1e873.jpeg~110x110.jpeg'
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
