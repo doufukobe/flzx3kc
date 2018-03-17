fc9b7044f28717cc522e3157deb078996add1938
Fu peidong <fupeidong.rd@bytedance.com>
2018-03-17 15:18:32 +0800

    rank and pk result

---
+++ src/pages/pk-result/pk-result.js
@@ -0,0 +1,79 @@
+// pages/pk-result/pk-result.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    isWin: true,
+    currentUser: {
+      name: '葱头豆瓣酱',
+      score: 270,
+      avatar: 'https://sf3-ttcdn-tos.pstatp.com/img/game-files/16393a4b709356457ad45282f6d1e873.jpeg~110x110.jpeg'
+    },
+    opponent: {
+      name: '葱头豆瓣豆瓣酱',
+      score: 170,
+      avatar: 'https://sf3-ttcdn-tos.pstatp.com/img/game-files/16393a4b709356457ad45282f6d1e873.jpeg~110x110.jpeg'
+    },
+    winTip: '金币+10',
+    pkTip: 'VS',
+    nextGame: '再来一局(10)'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
--- src/pages/rank/rank.js
+++ src/pages/rank/rank.js
@@ -5,7 +5,24 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    bannerSrc: '../../images/rank/rank_banner.png',
+    championSrc: '../../images/rank/champion_cup.png',
+    topThreeSrc: '../../images/rank/top_three_bg.png',
+    fisrtAvatarUrl: '../../images/rank/first.jpg',
+    secondAvatarUrl: '../../images/rank/first.jpg',
+    thirdAvatarUrl: '../../images/rank/first.jpg',
+    fisrtName: '雷子',
+    secondName: 'Jabaiak',
+    thirdName: '于童',
+    fisrtScore: '4320',
+    mineRank: '460',
+    mineName: '葱头豆瓣酱',
+    mineScore: '480',
+    userList: [{rank: '4', score: '300', name: '葱头豆瓣酱', avatar:'../../images/rank/first.jpg'},
+               {rank: '5', score: '250', name: '葱头豆瓣酱', avatar: '../../images/rank/first.jpg'},
+               {rank: '6', score: '200', name: '葱头豆瓣酱', avatar: '../../images/rank/first.jpg'},
+               {rank: '7', score: '150', name: '葱头豆瓣酱', avatar: '../../images/rank/first.jpg'},
+               {rank: '8', score: '100', name: '葱头豆瓣酱', avatar: '../../images/rank/first.jpg'}],
   },
 
   /**
