893a481bbf8756053b7f75bf57f0ed59af3adda3
Fu peidong <fupeidong.rd@bytedance.com>
2018-03-17 16:26:04 +0800

    attendance

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -5,6 +5,13 @@ const app = getApp()
 Page({
   data: {
     logoSrc: '../../images/index/logo_img.png',
+    isAttendanced: false,
+    attendanceTitle: '【签到领金币】',
+    attendanceTip: '已连续签到1天',
+    attendanceDay: 1,
+    attendanceDayList: [1,2,3,4,5,6,7],
+    attendanceCoinList: ['+10', '+20', '+30', '+40', '+50', '+60', '+100'],
+    takeAttendance: '立即签到',
   },
   //事件处理函数
   goTo(e) {
